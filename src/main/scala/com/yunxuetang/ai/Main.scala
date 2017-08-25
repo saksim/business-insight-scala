f1fa6f46dc17223c991fe9ab520c730bb00c14a4
wanglijun <juns1984@qq.com>
2017-08-25 11:30:25 +0800

    fix some bug

--- src/main/scala/com/yunxuetang/ai/Main.scala
+++ src/main/scala/com/yunxuetang/ai/Main.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.Executors
 
 import com.hankcs.hanlp.HanLP
 import com.hankcs.hanlp.dictionary.CustomDictionary
+import com.hankcs.hanlp.seg.Viterbi.ViterbiSegment
 import com.yunxuetang.ai.ability.abilitiesOf
 import com.yunxuetang.ai.algo.cutIntoSlice
 import com.yunxuetang.ai.repo.Slick.getReqItem
@@ -44,7 +45,7 @@ object Main {
     //    "熟知各大搜索引擎的排名原理和技术特性"
     //    "熟悉MICROSOFT OFFICE系列"
     //    "有团队合作精神"
-    //    "熟悉大数据处理架构"
+//        "熟悉大数据处理架构"
     //    "对大数据技术体系､关键技术､发展趋势有深刻的理解和认识"
     //    "能够独立搭建LINUX开发环境,如MYSQL、PHP、APACHE等常用软件"
 //    "少精通一门编程语言,如JAVA、C++或PYTHON"
@@ -55,7 +56,8 @@ object Main {
     //    "具备JAVA WEB､数据库开发基础者优先"
 //    "良好的沟通技巧和团队合作精神"
 //    "具有创意策划力",
-    "懂得肉食品冷藏存放技术"
+//    "懂得肉食品冷藏存放技术"
+        "发现并解决问题的能力"
   ).zipWithIndex.map {
     case (text, index) =>
       val id = index.toLong + 1L
@@ -83,9 +85,11 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    loadCustomDictionaries()
+//    loadCustomDictionaries()
+    val text = "王利军王小草?";  // 怎么可能噗哈哈！
+    println(HanLP.segment(text))
 //        printItems()
-        procDb()
+//        procDb()
 //    showAbilities()
   }
 
